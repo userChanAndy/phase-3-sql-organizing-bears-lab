ce3b3174eda3fe47c1eadbb00c9176817768f8b6
Andy Chan <andy.chan9930@gmail.com>
2022-02-25 14:01:37 -0500

    commit tree fb6124ee47c0537138a542671e7301686d8bd811

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "F", "Brown", "Mild", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES  ("Rowdy", 1, "M", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "M", "Black", "Tame", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergent Brown", 8, "F", "Brown", "Sweet", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 1, "F", "White", "Aggressive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "Black", "Mild", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "M", "White", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "Black", "Hyper Aggressive", 1);
